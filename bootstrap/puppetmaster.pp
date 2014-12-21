c3cdd623f72599b429b4fa6f2658e05e47eebecc
TNT Admin <tntadmin@tntinfra.net>
2014-12-21 13:41:52 +0100

    configure r10k

--- bootstrap/puppetmaster.pp
+++ bootstrap/puppetmaster.pp
@@ -1,4 +1,10 @@
 class puppetmaster {
+  $r10k_content = ":cachedir: '/var/cache/r10k'
+:sources:
+  :plops:
+    remote: 'https://github.com/tntinfra/puppet-tnt.git'
+    basedir: '/etc/puppet/environments'\n"
+
   package { 'puppetserver':
     ensure => 'installed',
   } -> 
@@ -6,4 +12,15 @@ class puppetmaster {
     ensure => 'running',
     enable => true,
   }
+
+  package { 'r10k':
+    ensure => 'installed',
+    provider => 'gem',
+  } ->
+  file { '/etc/r10k.yaml':
+    ensure => 'file',
+    content => $r10k_content,
+  }
 }
+
+include puppetmaster
